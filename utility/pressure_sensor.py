cb9a31da0d630762e96c5a5a91929d735dab93f7
necronaughtycon <tommyawallace@outlook.com>
2024-02-09 02:14:01 -0500

    ADC Class

--- utility/pressure_sensor.py
+++ utility/pressure_sensor.py
@@ -17,15 +17,16 @@ class ADC:
     '''
     _warnings = {'board': False, 'busio': False, 'ads': False}
 
-    def __init__(self, request_amount=60, adc_zero=15422.0, gain=1):
+    def __init__(self, amount=60, held=1, adc_zero=15422.0, gain=1):
         self._initialize_hardware()
         if self._hardware_available:
             i2c = busio.I2C(board.SCL, board.SDA)
             self._adc = ADS.ADS1115(i2c)
             self._channel = AnalogIn(self._adc, ADS.P0)
             self._adc.gain = gain
-        self.adc_zero = adc_zero
-        self.requests = deque(maxlen=request_amount)
+            self.adc_zero = adc_zero
+        self.requests = amount
+        self.requests_stored = deque(maxlen=held)
 
     @classmethod
     def _initialize_hardware(cls):
@@ -46,10 +47,9 @@ class ADC:
             cls._hardware_available = True
             from adafruit_ads1x15.analog_in import AnalogIn
         except ImportError as e:
-            if not cls._warnings_logged['ads']:
-                cls.log('warning', f'ADS1x15 library not available: {e}')
-                cls._warnings_logged['ads'] = True
-        if any(cls._warnings_logged):
+            print(f'ADS1x15 library not available: {e}')
+            cls._warnings['ads'] = True
+        if any(cls._warnings):
             _cls_hardware_available = False
 
     @staticmethod
@@ -65,72 +65,16 @@ class ADC:
         ''' Map a value from one range to another. '''
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-    def get_pressure(self) -> str:
-        ''' Get the pressure reading from the sensor. '''
-        if not self._hardware_available:
-            return '-99.9'
-        try:
-            for _ in range(30):
-                value = self._channel.value
-                pressure = round(self.map_value(value, self.adc_zero, 22864.0, 0.0, 20.8), 2)
-                self.pressure_readings.append(pressure)
-            avg_pressure = self._calculate_average(self.pressure_readings)
-            return f'{avg_pressure:.2f}' if f'{avg_pressure:.2f}' != '-0.00' else '0.00'
-        except IOError as e:
-            self.log('error', f'Error reading from ADC: {e}')
-            return '-99.9'
-
-    def calibrate(self):
-        ''' Recalibrate the pressure sensor. '''
+    def request_data(self) -> str:
+        ''' Send request to ADC. '''
         if not self._hardware_available:
             return 'ERR'
-        self.adc_zero = self._channel.value
-        self.pressure_readings.clear()
         try:
-            self.db.add_setting('adc_zero', self.adc_zero)
-        except ValueError as e:
-            self.log('warning', f'{e}')
-        return self.adc_zero
-
-    def pressure_simulator(self):
-        ''' For testing locally or without a pressure sensor connected '''
-        # Generate random incremental change to simulate gradual pressure changes.
-        change = random.uniform(-0.02, 0.02)
-        self._last_test_mode_pressure += change
-        # Ensure test pressure doesn't scale beyond range.
-        self._last_test_mode_pressure = max(-10.0, min(10.0, self._last_test_mode_pressure))
-        test_pressure = f'{self._last_test_mode_pressure:.2f}'
-        return test_pressure
-
-
-class PressureThresholds:
-    '''
-    Pressure Thresholds
-    -------------------
-    This class is used to store and update the pressure thresholds for the application.
-    '''
-
-    def __init__(self, low=-6.00, high=2.00, zero=0.15, variable=0.20):
-        self.low = low
-        self.high = high
-        self.zero = zero
-        self.variable = variable
-
-    def set_low(self, value):
-        ''' Set the low pressure threshold. '''
-        self.low = value
-
-    def set_high(self, value):
-        ''' Set the high pressure threshold. '''
-        self.high = value
-
-    def set_zero(self, value):
-        ''' Set the zero pressure threshold. '''
-        self.zero = value
-
-    def set_variable(self, value):
-        ''' Set the variable pressure threshold. '''
-        self.variable = value
-
-    def __str__(self):
-        return f'Thresholds(low={self.low}, high={self.high}, zero={self.zero}, variable={self.variable})'
+            for _ in range(self.requests):
+                value = self._channel.value
+                result = round(self.map_value(value, self.adc_zero, 22864.0, 0.0, 20.8), 2)
+                self.requests_stored.append(result)
+            avg_result = self._calculate_average(self.requests_stored)
+            return f'{avg_result:.2f}' if f'{avg_result:.2f}' != '-0.00' else '0.00'
+        except IOError:
+            return 'ERR'
