413f382bb1da0da60e198a65fd925752bf7012b6
necronaughtycon <tommyawallace@outlook.com>
2024-02-10 20:29:47 -0500

    Close!

--- app.py
+++ app.py
@@ -47,49 +47,43 @@ class MCPTestScreen(MDScreen):
 
 
 class ADCDialog:
-    ''' This class is a dialog that displays live statistics for an ongoing ADC test. '''
-    def __init__(self, **kwargs):
-        super(ADCDialog, self).__init__(**kwargs)
+    ''' This class handles the ADC test dialog. '''
+    def __init__(self, app, **kwargs):
+        self.app = app
         
-        # Dialog content layout
-        self.content = MDDialog(MDDialogHeadlineText(text='Live ADC Test Statistics'))
+        # Container setup.
         self.container = MDDialogContentContainer(orientation='vertical')
         self.container.add_widget(MDDivider())
-        self.payload = MDListItemSupportingText(
-            text='Payload Size: 0', 
-            halign='center'
-            )
+        self.payload = MDListItemSupportingText(text='Payload Size: 0', halign='center')
+        self.requests_received = MDListItemSupportingText(text='Requests Received: 0', halign='center')
         self.container.add_widget(MDListItem(self.payload))
-        self.requests_received = MDListItem(
-            MDListItemSupportingText(
-                text='Requests Received: 0',
-                halign='center'
-            )
-        )
-        self.container.add_widget(self.requests_received)
+        self.container.add_widget(MDListItem(self.requests_received))
         self.container.add_widget(MDDivider())
-        self.content.add_widget(self.container)
+
+        # Button setup.
         self.button_container = MDDialogButtonContainer()
         self.button = MDButton(
-            style='elevated',
-            theme_width='Custom',
-            size_hint_y=None,
-            height='48dp',
-            radius=7,
-            size_hint_x=.5
+            style='elevated', theme_width='Custom', size_hint_y=None,
+            height='48dp', radius=7, size_hint_x=.5,
+            on_press=self.app.stop_adc_test
         )
-        self.button.bind(on_release=self.stop_adc_test)
         self.button_text = MDButtonText(
-            text='Stop',
-            font_style='Title',
-            role='large',
+            text='Stop', font_style='Title', role='large',
             pos_hint={'center_x': .5, 'center_y': .5}
         )
         self.button.add_widget(self.button_text)
         self.button_container.add_widget(Widget(size_hint_x=.25))
         self.button_container.add_widget(self.button)
         self.button_container.add_widget(Widget(size_hint_x=.25))
-        self.content.add_widget(self.button_container)
+        
+        # Dialog setup.
+        self.dialog = MDDialog(
+            MDDialogHeadlineText(
+                text='Live ADC Test Statistics'
+            ),
+            self.container,
+            self.button_container
+        )
     
     def update_payload_size(self, size):
         self.payload.text = f'Payload Size: {size}'
@@ -98,15 +92,10 @@ class ADCDialog:
         self.requests_received.text = f'Requests Sent: {requests}'
     
     def open(self):
-        self.content.open()
+        self.dialog.open()
     
     def close(self):
-        self.content.dismiss()
-    
-    def stop_adc_test(self, instance):
-        # Implement stopping logic here, possibly using a callback to the parent app
-        self.close()
-    
+        self.dialog.dismiss()
 
 
 class StressTestApp(MDApp):
@@ -159,17 +148,14 @@ class StressTestApp(MDApp):
         self.adc_task = Clock.schedule_interval(lambda dt: self.handle_adc_data(data_held), int(frequency))
     
     def show_adc_dialog_new(self):
+        ''' Display a dialog with live statistics for the ongoing ADC test. '''
         if not hasattr(self, 'adc_dialog'):
-            self.adc_dialog = ADCDialog()
+            self.adc_dialog = ADCDialog(self)
             self.adc_dialog.button.bind(on_release=self.stop_adc_test)
     
         self.adc_dialog.update_payload_size(self.adc_requests)
         self.adc_dialog.update_requests_sent(self.adc_requests_received)
         self.adc_dialog.open()
-
-
-
-
 
     def handle_adc_data(self, data_held):
         ''' Handle the ADC data. '''
@@ -183,7 +169,7 @@ class StressTestApp(MDApp):
         print(f'Requests sent: {requests_sent}')
         print(f'Data held: {len(data_held)}')
 
-    def stop_adc_test(self):
+    def stop_adc_test(self, instance=None):
         ''' Stop and unschedule the ADC test. '''
         if self.adc_task:
             self.adc_task.cancel()
@@ -191,43 +177,6 @@ class StressTestApp(MDApp):
             self.adc_requests = 0
             self.adc_stored.clear()
 
-    def show_adc_dialog(self):
-        ''' Display a dialog with live statistics for the ongoing ADC test. '''
-        MDDialog(
-            MDDialogHeadlineText(text="Live ADC Test Statistics"),
-            MDDialogContentContainer(
-                MDDivider(),
-                MDListItem(
-                    MDListItemSupportingText(
-                        text=f'Payload Size: {self.adc_requests}',
-                        halign='center'
-                        )
-                ),
-                MDListItem(
-                    MDListItemSupportingText(text=f'Requests Sent: {self.adc_requests_received}')
-                ),
-                MDDivider(),
-                orientation = 'vertical'
-            ),
-            MDDialogButtonContainer(
-                Widget(size_hint_x=.25),
-                MDButton(
-                    MDButtonText(
-                        text='Stop',
-                        font_style='Title',
-                        role='large',
-                        pos_hint={'center_x': .5, 'center_y': .5}
-                        ),
-                    style='elevated',
-                    theme_width='Custom',
-                    size_hint_y=None,
-                    height='48dp',
-                    radius=7,
-                    size_hint_x=.5
-                ),
-                Widget(size_hint_x=.25)
-            ),
-        ).open()
 
 if __name__ == '__main__':
     StressTestApp().run()
