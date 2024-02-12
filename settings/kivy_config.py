fbdfe7bb9fe09cffdcf9f968d5585b34136d24d6
necronaughtycon <tommyawallace@outlook.com>
2024-02-11 20:48:45 -0500

    Added settings folder for consistency

---
+++ settings/kivy_config.py
@@ -0,0 +1,12 @@
+''' Settings required to run on the Comfile. '''
+
+from kivy.config import Config
+
+Config.set('graphics', 'borderless', '1')
+Config.set('graphics', 'height', '480')
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'show_cursor', '0')
+Config.set('graphics', 'show_cursor', '0')
+Config.set('graphics', 'width', '800')
+
+Config.write()
