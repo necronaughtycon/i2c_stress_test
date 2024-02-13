8f561dfb81ed5b915b992147298a2969ad10f051
necronaughtycon <tommyawallace@outlook.com>
2024-02-13 16:27:34 -0500

    +

--- utility/adc_config.py
+++ utility/adc_config.py
@@ -38,6 +38,7 @@ class ADC:
 
     def __init__(self, gain=1):
         self._hardware_initialized = False
+        self._stop = False
         if busio is None or board is None or ADS is None or AnalogIn is None:
             return
         i2c = busio.I2C(board.SCL, board.SDA)
@@ -46,14 +47,22 @@ class ADC:
         self._adc.gain = gain
         self._hardware_initialized = True
 
-    def read_adc(self, delay) -> str:
+    def read_adc(self, requests, delay) -> str:
         ''' Send request to ADC. '''
         if not self._hardware_initialized:
             return 'ERR'
-        try:
-            value = self._channel.value
-            time.sleep(delay)
-            return str(value)
-            # return f'{value:.2f}' if f'{value:.2f}' != '-0.00' else '0.00'
-        except IOError:
-            return 'ERR'
+        if not self._stop:
+            for _ in range(requests):
+                try:
+                    value = self._channel.value
+                    time.sleep(delay)
+                    return str(value)
+                    # return f'{value:.2f}' if f'{value:.2f}' != '-0.00' else '0.00'
+                except IOError:
+                    return 'ERR'
+                if self._stop:
+                    break
+
+    def stop(self):
+        ''' Stop the ADC. '''
+        self._stop = True
